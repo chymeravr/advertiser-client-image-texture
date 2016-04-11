83cbb487004304fa299d8eb60163760f7d9c0976
p12sushilkm <sushilm.iitb@gmail.com>
2016-04-11 19:43:26 +0530

    Modifying DTO to get advertiserid and adid

--- src/main/java/co/chimeralabs/advertiser/client/apiDTO/ImageTextureAdDTO.java
+++ src/main/java/co/chimeralabs/advertiser/client/apiDTO/ImageTextureAdDTO.java
@@ -1,12 +1,30 @@
 package co.chimeralabs.advertiser.client.apiDTO;
 
 public class ImageTextureAdDTO {
+	private String advertiserId;
+	private String adId;
 	private String adResourceIdentifier;
 	private String adResourceFormat;
 	private String errorMsg;
 	
 	public ImageTextureAdDTO(){
 
+	}
+
+	public String getAdvertiserId() {
+		return advertiserId;
+	}
+
+	public void setAdvertiserId(String advertiserId) {
+		this.advertiserId = advertiserId;
+	}
+
+	public String getAdId() {
+		return adId;
+	}
+
+	public void setAdId(String adId) {
+		this.adId = adId;
 	}
 
 	public String getAdResourceIdentifier() {
